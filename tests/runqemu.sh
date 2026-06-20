ac1f538726b8e9e6a6521e9f1736b6ca589abf19
Cornelia Schulz <cornelia.schulz@web.de>
2026-06-20 23:17:01 +0200

    [#3] add buildroot config and qemu start script

---
+++ tests/runqemu.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Script to run qemu:
+#   Device needs access to 192.168.0.xx, so we can't use an emulated Pi Zero (has no networking interface)
+#   Instead, we switch to '-M versatilepb' board and use a virtual network bridge on the host
+
+# Setup tap0 interface (host):
+#   sudo ip tuntap add dev tap0 mode tap user $(whoami)     # create interface
+#   sudo ip link set tap0 up                                # activate interface
+#   sudo ip addr add 192.168.0.99/24 dev tap0               # manually assign IP
+
+# Configure eth0 interface (qemu):
+#   ip addr add 192.168.0.100/24 dev eth0
+#   ip link set eth0 up
+
+CURRENT_DIR=$(dirname ${BASH_SOURCE[0]})
+BUILDROOT_DIR=${CURRENT_DIR}/../buildroot/output/images
+
+qemu-system-arm \
+  -M versatilepb \
+  -kernel ${BUILDROOT_DIR}/zImage \
+  -dtb ${BUILDROOT_DIR}/versatile-pb.dtb \
+  -drive file=${BUILDROOT_DIR}/rootfs.ext2,if=scsi,format=raw \
+  -append "rootwait root=/dev/sda console=ttyAMA0,115200" \
+  -net nic,model=rtl8139 \
+  -net user \
+  -nographic
