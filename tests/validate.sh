481ca7ec6886e9a3a54d8ffa5b9deb1b5421d2b2
Cornelia Schulz <cornelia.schulz@web.de>
2026-06-22 15:18:56 +0200

    [#3] export function and use colorful prints

--- tests/validate.sh
+++ tests/validate.sh
@@ -3,14 +3,27 @@
 TEST_DIR=$(dirname ${BASH_SOURCE[0]})
 REPO_DIR=$(dirname ${TEST_DIR})
 
+# define different colors
+# See https://stackoverflow.com/questions/5947742/how-to-change-the-output-color-of-echo-in-linux
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+NC='\033[0m' # No Color
 
 ##################
 # helper functions
 
+# print $color $text: print text in a specific color
+print(){
+  color=$1
+  text=$2
+  printf "${color}>>> ${text} <<<${NC}\n"
+}
+
 # validate $rc: if error occurs, print it and exit
 validate(){
   if [ $1 -ne 0 ]; then
-    echo "Test failed with error: $1"
+    print $RED "Test failed with error: $1"
     exit 1
   fi
 }
@@ -20,14 +33,15 @@ ssh_cmd(){
 	cmd=$1
 	sshpass -p 'root' ssh -o StrictHostKeyChecking=no root@localhost -p 2222 ${cmd}
 }
+export -f ssh_cmd
 
 # try_cmd_for $time $sleep $cmd: repeatedly execute $cmd until it succeeds, with timeout and sleep
 try_cmd_for(){
   timeout_duration=$1
   sleep_duration=$2
   cmd=$3
-  echo "Try for $timeout_duration: $cmd"
-  timeout $timeout_duration bash -c "until $cmd; do echo $cmd; sleep $sleep_duration; done"
+  print $YELLOW "Try for $timeout_duration: $cmd"
+  timeout $timeout_duration bash -c "until $cmd; do sleep $sleep_duration; done"
 }
 
 
@@ -36,7 +50,7 @@ try_cmd_for(){
 
 # build buildroot image with qemu settings
 build_image(){
-  echo "Build buildroot image with qemu config"
+  print $YELLOW "Build buildroot image with qemu config"
   pushd $REPO_DIR
 
   # make config QEMU_BUILD=true
@@ -49,38 +63,39 @@ build_image(){
   validate $?
 
   popd
-  echo "Built buildroot image with qemu config"
+  print $GREEN "Built buildroot image with qemu config"
 }
 
 # boot image with qemu
 start_qemu(){
-  echo "Start qemu instance in background"
+  print $YELLOW "Start qemu instance in background"
   ${TEST_DIR}/runqemu.sh &
 
   validate $?
-  echo "Wait 30s for qemu instance to be up"
+  print $YELLOW "Wait 30s for qemu instance to be up"
   sleep 30
-  echo "Started qemu instance in background"
+  print $GREEN "Started qemu instance in background"
 }
 
 # kill qemu instance
 stop_qemu(){
-  echo "Stop qemu instance"
+  print $YELLOW "Stop qemu instance"
   killall -9 qemu-system-arm
 
   validate $?
-  echo "Stopped qemu instance"
+  sleep 1
+  print $GREEN "Stopped qemu instance"
 }
 
 # log in via ssh
 login_ssh(){
-  echo "Log in via ssh"
+  print $YELLOW "Log in via ssh"
 
   ssh-keygen -f "${HOME}/.ssh/known_hosts" -R "[localhost]:2222"
   try_cmd_for 180s 5 "ssh_cmd 'exit'"
 
   validate $?
-  echo "Logged in via ssh"
+  print $GREEN "Logged in via ssh"
 }
 
 
