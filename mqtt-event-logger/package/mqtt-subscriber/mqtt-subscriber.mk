1c571f5b096bbfbed8051ea1219d1690fa3c90d6
Cornelia Schulz <cornelia.schulz@web.de>
2026-06-27 17:57:03 +0200

    [#9] try to fix make/cmake build and install targets

--- mqtt-event-logger/package/mqtt-subscriber/mqtt-subscriber.mk
+++ mqtt-event-logger/package/mqtt-subscriber/mqtt-subscriber.mk
@@ -11,15 +11,20 @@ MQTT_SUBSCRIBER_SITE_METHOD = local
 
 # add openssl as dependency (required for paho mqtt library)
 MQTT_SUBSCRIBER_DEPENDENCIES = openssl libopenssl
+MQTT_SUBSCRIBER_CMAKE_BUILD_OPTIONS = " \
+	-DOPENSSL_LIB_SEARCH_PATH=$(TARGET_DIR)/usr/lib \
+    -DOPENSSL_INC_SEARCH_PATH=$(TARGET_DIR)/usr/include/openssl;$(TARGET_DIR)/usr/include \
+	-DCMAKE_TOOLCHAIN_FILE=$(@D)/paho.mqtt.c/cmake/toolchain.linux-arm11.cmake"
 
 # define build commands
 define MQTT_SUBSCRIBER_BUILD_CMDS
-	$(MAKE)
+	$(TARGET_MAKE_ENV) $(MAKE) clean
+	$(TARGET_MAKE_ENV) $(MAKE) BUILD_DIR=. CMAKE_BUILD_OPTIONS="$(MQTT_SUBSCRIBER_CMAKE_BUILD_OPTIONS)"
 endef
 
 # define install commands
 define MQTT_SUBSCRIBER_INSTALL_TARGET_CMDS
-	$(MAKE) install INSTALL_CMD=$(INSTALL) BUILD_DIR=$(@D) TARGET_DIR=$(TARGET_DIR)/usr
+	$(MAKE) install INSTALL_CMD=$(INSTALL) BUILD_DIR=. TARGET_DIR=$(TARGET_DIR)/usr
 endef
 
 $(eval $(generic-package))
