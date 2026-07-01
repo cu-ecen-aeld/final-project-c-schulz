e270199461798466e89734cf51e2f9576519374e
Cornelia Schulz <cornelia.schulz@web.de>
2026-07-01 22:07:28 +0200

    [#11] add basic rust package structure

--- mqtt-event-logger/package/mqtt-char-driver/mqtt-char-driver.mk
+++ mqtt-event-logger/package/mqtt-char-driver/mqtt-char-driver.mk
@@ -9,9 +9,6 @@ MQTT_CHAR_DRIVER_VERSION = v1.0.0
 MQTT_CHAR_DRIVER_SITE = $(BR2_EXTERNAL_MQTT_EVENT_LOGGER_PATH)/package/mqtt-char-driver/src
 MQTT_CHAR_DRIVER_SITE_METHOD = local
 
-# specify the directory containing the kernel module
-#MQTT_CHAR_DRIVER_MODULE_SUBDIRS = src	# TODO
-
-# execute cargo to build package
+# execute make to build package
 $(eval $(kernel-module))
-$(eval $(cargo-package))
+$(eval $(generic-package))
