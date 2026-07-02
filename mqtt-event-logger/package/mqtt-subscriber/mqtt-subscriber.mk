44a8c7117f99a0fa7d7b660c9d83b3435bae0010
Cornelia Schulz <cornelia.schulz@web.de>
2026-07-02 11:44:53 +0200

    [#17] add start-stop script

--- mqtt-event-logger/package/mqtt-subscriber/mqtt-subscriber.mk
+++ mqtt-event-logger/package/mqtt-subscriber/mqtt-subscriber.mk
@@ -14,13 +14,13 @@ MQTT_SUBSCRIBER_DEPENDENCIES = openssl
 
 # setup start-stop script and configure mqtt hostname
 ifeq ($(BR2_PACKAGE_MQTT_SUBSCRIBER),y)
-define MQTT_SUBSCRIBER_CONFIGURE_MQTT_SUBSCRIBER
-	$(INSTALL) -m 0755 $(@D)/mqtt_subscriber_start-stop                $(TARGET_DIR)/etc/init.d/S99mqtt_subscriber
-	$(SED) "s|%HOST%|\"$(BR2_PACKAGE_MQTT_SUBSCRIBER_MQTT_HOST)\"|g"   $(TARGET_DIR)/etc/init.d/S99mqtt_subscriber
-	$(SED) "s|%TOPIC%|\"$(BR2_PACKAGE_MQTT_SUBSCRIBER_MQTT_TOPIC)\"|g" $(TARGET_DIR)/etc/init.d/S99mqtt_subscriber
+define MQTT_SUBSCRIBER_INSTALL_INIT_SCRIPT
+	$(INSTALL) -m 0755 $(@D)/mqtt_subscriber_start-stop                $(TARGET_DIR)/etc/init.d/S99mqtt-subscriber
+	$(SED) "s|%HOST%|\"$(BR2_PACKAGE_MQTT_SUBSCRIBER_MQTT_HOST)\"|g"   $(TARGET_DIR)/etc/init.d/S99mqtt-subscriber
+	$(SED) "s|%TOPIC%|\"$(BR2_PACKAGE_MQTT_SUBSCRIBER_MQTT_TOPIC)\"|g" $(TARGET_DIR)/etc/init.d/S99mqtt-subscriber
 endef
 
-MQTT_SUBSCRIBER_POST_INSTALL_TARGET_HOOKS += MQTT_SUBSCRIBER_CONFIGURE_MQTT_SUBSCRIBER
+MQTT_SUBSCRIBER_POST_INSTALL_TARGET_HOOKS += MQTT_SUBSCRIBER_INSTALL_INIT_SCRIPT
 endif
 
 
