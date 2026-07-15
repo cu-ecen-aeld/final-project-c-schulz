48abc2fe9f8c8b64228135e467e6c4b2fb6767ed
Cornelia Schulz <cornelia.schulz@web.de>
2026-07-15 12:07:09 +0200

    [#21] tests stats

--- mqtt-event-logger/package/mqttlogctl/src/src/main.rs
+++ mqtt-event-logger/package/mqttlogctl/src/src/main.rs
@@ -1,3 +1,11 @@
+mod ioctl;
+mod mqttlog;
+
 fn main() {
     println!("mqttlogctl");
+
+    // print stats
+    let dev = mqttlog::MqttLog::open().unwrap();
+    let stats = dev.stats().unwrap();
+    println!("{:#?}", stats);
 }
